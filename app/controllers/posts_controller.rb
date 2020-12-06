406d3f4a057a0ccd789ef784bf012602f5747d6f
tom-ash <tomasz.bogusz@outlook.com>
2020-12-06 09:47:58 +0100

    ADD posts as json

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,12 +8,16 @@ class PostsController < ApplicationController
     return bad_request unless user_validated?
 
     post = Post.find_by(name: params[:name]) || Post.new
+
+    meta = params[:meta] || {"en": "", "pl": ""}
+    url = params[:url] || {"en": "", "pl": ""}
+    title = params[:url] || {"en": "", "pl": ""}
+
     post.assign_attributes(
       author_id: @user.id,
       name: params[:name],
       meta: params[:meta],
       url: params[:url],
-      title: params[:title],
       body: params[:body]
     )
 
@@ -23,7 +27,7 @@ class PostsController < ApplicationController
   end
 
   def show
-    render json: Post.find_by(name: params[:name]).slice(:body, :title).merge(
+    render json: Post.find_by(name: params[:name]).slice(:name, :title, :body).merge(
       scalableVectorGraphics: ScalableVectorGraphic.all
     )
   end
