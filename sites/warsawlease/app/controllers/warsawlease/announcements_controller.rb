afdd7192457def924598081b0edb0cf1f6a38cd9
tom-ash <tomasz.bogusz@outlook.com>
2021-04-13 19:23:37 +0200

    FIX announcements api refactor

--- sites/warsawlease/app/controllers/warsawlease/announcements_controller.rb
+++ sites/warsawlease/app/controllers/warsawlease/announcements_controller.rb
@@ -1,11 +1,6 @@
 module Warsawlease
   class AnnouncementsController < Warsawlease::ApplicationController
     include Sites
-    include AnnouncementsAttributes
-    include AnnouncementsIndex
-    include AnnouncementsShow
-    include AnnouncementsUpdate
-    include AnnouncementsDestroy
     include Responses
   end
 end
