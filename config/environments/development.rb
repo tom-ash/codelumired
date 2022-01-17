64ab0baeba553eb69a24d5dd882ff7d6974b7f73
tom-ash <tomasz.bogusz@outlook.com>
2022-01-17 08:56:45 +0100

    shared | Remove redundant constants

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,11 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  ENV['WARSAWLEASE_ROOT_DOMAIN'] = 'http://local.warsawlease.pl:8080'
-  ENV['SOUNDOFIT_ROOT_DOMAIN'] = 'http://local.soundof.it:8080'
-
-  ENV['MAPAWYNAJMU_PL_NAME'] = 'mapawynajmu.pl'
-
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
