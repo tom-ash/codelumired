e57777e9ba57a3da3f1407c5c20d4c535572de02
tom-ash <tomasz.bogusz@outlook.com>
2022-03-19 07:15:10 +0100

    soundof.IT | Add Skill model

---
+++ sites/soundof_it/app/models/soundof_it/skill.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module SoundofIt
+  class Skill < ApplicationRecord
+    establish_connection :soundof_it
+
+    self.table_name = 'skills'
+
+    # has_and_belongs_to_many :jobs
+
+    validates :added_by_id, presence: true
+    validates :name, presence: true, uniqueness: true # TODO: Add unique index to :name.
+    validates :type, presence: true
+  end
+end
