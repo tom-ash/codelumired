4f3d2ec1646983d968cef6ff700f03302ee1ec21
tom-ash <tomasz.bogusz@outlook.com>
2022-03-19 07:27:22 +0100

    soundof.IT | Add Job model

---
+++ sites/soundof_it/app/models/soundof_it/job.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module SoundofIt
+  class Job < ApplicationRecord
+    establish_connection :soundof_it
+
+    self.table_name = 'jobs'
+
+    has_and_belongs_to_many :skills
+
+    # Non-validated fields: sublocality, street, description
+    validates :user, presence: true
+    validates :views, presence: true, numericality: true
+    validates :company_name, presence: true
+    validates :company_size, presence: true
+    validates :remote, inclusion: { in: [true, false] }
+    validates :hybrid, inclusion: { in: [true, false] }
+    validates :office, inclusion: { in: [true, false] }
+    validates :country, presence: true
+    validates :locality, presence: true
+    validates :employment, inclusion: { in: [true, false] }
+    validates :b2b, inclusion: { in: [true, false] }
+    # Add :mandate.
+    validates :employment_min, numericality: true
+    validates :employment_max, numericality: true
+    validates :b2b_min, numericality: true
+    validates :b2b_max, numericality: true
+  end
+end
