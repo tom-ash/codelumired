0a55fe858e73a18825ab612a4fff9161b493deb2
tom-ash <tomasz.bogusz@outlook.com>
2022-04-03 08:53:59 +0200

    soundof.IT | Add job/create location

--- sites/soundof_it/app/lib/soundof_it/api/methods/job/create/as_user.rb
+++ sites/soundof_it/app/lib/soundof_it/api/methods/job/create/as_user.rb
@@ -10,12 +10,18 @@ module SoundofIt
 
             params do
               requires :selected_skills, type: Array
+              requires :remote, type: Boolean
+              requires :hybrid, type: Boolean
+              requires :office, type: Boolean
             end
             post do
               ::SoundofIt::Commands::Job::Create.new(
                 user_id: current_user.id,
                 attrs: {
-                  selected_skills: params[:selected_skills]
+                  selected_skills: params[:selected_skills],
+                  remote: params[:remote],
+                  hybrid: params[:hybrid],
+                  office: params[:office]
                 }
               ).call
             end
