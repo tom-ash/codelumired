ea316715100012a7935a88f6cbca073298e76b06
tom-ash <tomasz.bogusz@outlook.com>
2022-05-06 08:34:10 +0200

    soundof.IT | page/index add :tutorials to Appender

--- sites/soundof_it/app/lib/soundof_it/api/tracks/root/appender.rb
+++ sites/soundof_it/app/lib/soundof_it/api/tracks/root/appender.rb
@@ -10,8 +10,20 @@ module SoundofIt
 
           private
 
+          def merge_state
+            state.merge!(
+              'page/index': {
+                tutorials: tutorials
+              }
+            )
+          end
+
           def page_name
             @page_name ||= 'root'
+          end
+
+          def tutorials
+            ::SoundofIt::Page.where(category: 'tutorials').pluck(:link_data)
           end
         end
       end
