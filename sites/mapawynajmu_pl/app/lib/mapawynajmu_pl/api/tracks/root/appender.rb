af500e8816a27d2d1f02f6f03ec57358fa007a8d
tom-ash <tomasz.bogusz@outlook.com>
2022-06-11 06:51:53 +0200

    mapawynajmu.pl | page/index add PageIndexer

--- sites/mapawynajmu_pl/app/lib/mapawynajmu_pl/api/tracks/root/appender.rb
+++ sites/mapawynajmu_pl/app/lib/mapawynajmu_pl/api/tracks/root/appender.rb
@@ -15,7 +15,10 @@ module MapawynajmuPl
           def merge_state
             state.merge!(
               'announcement/index/data': data,
-              'announcement/index/inputs': inputs
+              'announcement/index/inputs': inputs,
+              'page/index': {
+                articles: articles
+              }
             )
           end
 
@@ -32,6 +35,22 @@ module MapawynajmuPl
               return unless url.in?(['/', 'en'])
 
               'root'
+            end
+          end
+
+          def articles
+            ::MapawynajmuPl::Page.where(category: 'articles').map do |page|
+              {
+                title: page.title,
+                description: page.description,
+                category: page.category,
+                subcategory: page.subcategory,
+                hrefLang: page.lang,
+                pathname: page.url,
+                modifiedOn: page.modified_on,
+                logo: page.link_data['logo'],
+                image: page.link_data['image']
+              }
             end
           end
         end
