91e3e7f863ebebab2bcdadc92c5abffa7d35c1a8
tom-ash <tomasz.bogusz@outlook.com>
2022-08-09 17:04:36 +0200

    mapawynajmu.pl | add Price Filter

--- sites/mapawynajmu_pl/app/lib/mapawynajmu_pl/api/tracks/helpers/filters.rb
+++ sites/mapawynajmu_pl/app/lib/mapawynajmu_pl/api/tracks/helpers/filters.rb
@@ -10,7 +10,9 @@ module MapawynajmuPl
           def filters
             @filters ||= {
               area_min: area_min,
-              area_max: area_max
+              area_max: area_max,
+              price_min: price_min,
+              price_max: price_max
             }
           end
 
@@ -24,12 +26,26 @@ module MapawynajmuPl
                           params[:powierzchnia_max]
           end
 
+          def price_min
+            @price_min ||= params[:cena_min] ||
+                           params[:price_min]
+          end
+
+          def price_max
+            @price_max ||= params[:cena_maks] ||
+                           params[:price_max]
+          end
+
           def inputs
             {
               area_min: area_min.to_s,
               area_max: area_max.to_s,
               area_min_input: area_min.to_s,
-              area_max_input: area_max.to_s
+              area_max_input: area_max.to_s,
+              price_min: price_min.to_s,
+              price_min_input: price_min.to_s,
+              price_max: price_max.to_s,
+              price_max_input: price_max.to_s
             }
           end
         end
--- sites/mapawynajmu_pl/app/lib/mapawynajmu_pl/queries/announcement/index/visitor.rb
+++ sites/mapawynajmu_pl/app/lib/mapawynajmu_pl/queries/announcement/index/visitor.rb
@@ -5,10 +5,12 @@ module MapawynajmuPl
     module Announcement
       module Index
         class Visitor
-          def initialize(category: nil, area_min: nil, area_max: nil)
+          def initialize(category: nil, area_min: nil, area_max: nil, price_min: nil, price_max: nil)
             @category = category
             @area_min = area_min
             @area_max = area_max
+            @price_min = price_min
+            @price_max = price_max
           end
 
           def call
@@ -17,12 +19,15 @@ module MapawynajmuPl
 
           private
 
-          attr_reader :category, :area_min, :area_max
+          attr_reader :category, :area_min, :area_max, :price_min, :price_max
 
           def announcements
             @announcement ||= ::MapawynajmuPl::Announcement.where(search_params).order('points DESC').order('active_until DESC')
             @announcement = @announcement.where('area >= ?', area_min) if area_min.present?
             @announcement = @announcement.where('area <= ?', area_max) if area_max.present?
+
+            @announcement = @announcement.where('gross_rent_amount >= ?', price_min) if price_min.present?
+            @announcement = @announcement.where('gross_rent_amount <= ?', price_max) if price_max.present?
 
             @announcement
           end
