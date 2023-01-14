0484f8a441c7ed2a033856ba442cb26eb5950a61
tom-ash <tomasz.bogusz@outlook.com>
2023-01-14 11:07:35 +0100

    shared > user/edit > Add GenericAttribute extended

--- spec/app/lib/api/user/authorize/access_token_spec.rb
+++ spec/app/lib/api/user/authorize/access_token_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe ::Api::User::Authorize::AccessToken do
-  let(:account_type) { 'personal' }
+  let(:account_type) { 'private' }
   let(:first_name) { 'Gandalf' }
   let(:access_token) { 'ba79099602b5ca78b2c44b652ebee8c8ff0c522e93ab8a7a39ceeb60c3dc40d5fe9984cb45fe4464b4d48c3d6eba70040e6c1f9987c7177f259beee19dcd1a1e' }
   let(:encrypted_access_token) { "jklIUPH3ggQD52/IMTyaoW1SL/bc3cRun0wstob6401/yLPDfIEC8pDWopbf\nS5+GoMAUPDKc+FV0y12ahoDTgrAekevOcIigfDIe8FwmljRF9rk6vDrhFo7u\nvp1VhNEuyVbCZEceIjqmkuz5RECIs83AjoKe2AB4Lnl9iqXBRskGz3Rj6pn9\nsTiFjdQfdPx/jQX1q5VKe8KgpCAbM89hefJ4CgEzHWW9t/2LZ3S0M9hZ4d0b\nKHBpqhdjKqzPg9x5\n" }
@@ -22,7 +22,7 @@ RSpec.describe ::Api::User::Authorize::AccessToken do
         expect(JSON.parse(response.body).symbolize_keys).to eq(
           accountType: account_type,
           accessToken: access_token,
-          name: first_name
+          name: first_name,
         )
       end
     end
