0a29b6e0bfe6685c3eb009892de44e26ed914bf8
tom-ash <tomasz.bogusz@outlook.com>
2023-01-26 16:53:24 +0100

    mapawynajmu.pl > root > Add partner paths

--- sites/mapawynajmu_pl/app/lib/mapawynajmu_pl/api/tracks/root/linker.rb
+++ sites/mapawynajmu_pl/app/lib/mapawynajmu_pl/api/tracks/root/linker.rb
@@ -8,6 +8,9 @@ module MapawynajmuPl
           include ::MapawynajmuPl::Api::Tracks::Helpers::Linker
           include ::MapawynajmuPl::Api::Tracks::Root::Meta
 
+          ROOT_PL = '/'
+          ROOT_EN = 'en'
+
           def initialize(lang, url = nil)
             @lang = lang
             @url = url
@@ -17,31 +20,66 @@ module MapawynajmuPl
 
           def call
             {
-              pl: { path: '/' },
-              en: { path: 'en' },
+              pl: { path: ROOT_PL },
+              en: { path: ROOT_EN },
             }[lang]
           end
 
           def lang_links
-            if category.present?
-              return {
-                'current/pl': { path: category_link(::MapawynajmuPl::Announcement::CATEGORIES[category], :pl) },
-                'current/en': { path: category_link(::MapawynajmuPl::Announcement::CATEGORIES[category], :en) },
-              }
-            end
+            return partner_and_category_paths if partner.present? && category.present?
+            return partner_paths if partner.present?
+            return category_paths if category.present?
 
+            root_paths
+          end
+
+          def partner_and_category_paths
             {
-              'current/pl': { path: lang_prefix(:pl).chomp('/') },
-              'current/en': { path: lang_prefix(:en) },
+              'current/pl': { path: "#{partner_path(:pl)}/#{category_path(:pl)}" },
+              'current/en': { path: "#{partner_path(:en)}/#{category_path(:en)}" },
             }
+          end
+
+          def partner_paths
+            {
+              'current/pl': { path: partner_path(:pl) },
+              'current/en': { path: partner_path(:en) },
+            }
+          end
+
+          def category_paths
+            {
+              'current/pl': { path: category_path(:pl) },
+              'current/en': { path: category_path(:en) },
+            }
+          end
+
+          def root_paths
+            {
+              'current/pl': { path: ROOT_PL.chomp('/') },
+              'current/en': { path: ROOT_EN },
+            }
+          end
+
+          def partner_path(lang)
+            {
+              pl: "partnerzy/#{partner.urlified_business_name}",
+              en: "partners/#{partner.urlified_business_name}",
+            }[lang]
+          end
+
+          def category_path(lang)
+            category_link(::MapawynajmuPl::Announcement::CATEGORIES[category], lang)
           end
 
           def category_links
             category_links_hash = {}
 
             ::MapawynajmuPl::Announcement::CATEGORIES.each_value do |category|
+              category_path = category_link(category, lang)
+
               category_links_hash["root/#{category[:trackified]}"] = {
-                path: category_link(category, lang),
+                path: partner.present? ? "#{partner_path(lang)}/#{category_path}" : category_path,
               }
             end
 
@@ -52,10 +90,6 @@ module MapawynajmuPl
 
           def category_link(category, lang)
             "#{category_prefix(lang)}/#{category[:urlified_plural][lang]}"
-          end
-
-          def lang_prefix(lang)
-            lang == :pl ? '' : 'en'
           end
 
           def category_prefix(lang)
