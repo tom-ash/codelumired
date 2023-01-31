b224a445a9d120fe41b853010dbf6a5f2e742c5e
tom-ash <tomasz.bogusz@outlook.com>
2023-01-31 07:34:38 +0100

    mapawynajmu.pl > listing/update > fix rent per sqm

--- sites/mapawynajmu_pl/app/lib/mapawynajmu_pl/commands/announcement/update.rb
+++ sites/mapawynajmu_pl/app/lib/mapawynajmu_pl/commands/announcement/update.rb
@@ -44,9 +44,11 @@ module MapawynajmuPl
 
         def parsed_attrs
           direct_attrs.merge(
+            **net_rent_amount_object,
+            **net_rent_amount_per_sqm_object,
+            **gross_rent_amount_object,
+            **gross_rent_amount_per_sqm_object,
             **active_until,
-            **net_rent_amount_per_sqm,
-            **gross_rent_amount_per_sqm,
           )
         end
 
@@ -54,26 +56,28 @@ module MapawynajmuPl
           attrs.slice(*DIRECT_ATTR_NAMES)
         end
 
+        def net_rent_amount_object
+          { net_rent_amount: net_rent_amount }
+        end
+
+        def net_rent_amount_per_sqm_object
+          return {} if net_rent_amount.blank? || area.blank?
+
+          { net_rent_amount_per_sqm: (net_rent_amount / area).ceil(2) }
+        end
+
+        def gross_rent_amount_object
+          { gross_rent_amount: gross_rent_amount }
+        end
+
+        def gross_rent_amount_per_sqm_object
+          return {} if gross_rent_amount.blank? || area.blank?
+
+          { gross_rent_amount_per_sqm: (gross_rent_amount / area).ceil(2) }
+        end
+
         def active_until
           { active_until: Date.today + 60.days }
-        end
-
-        def net_rent_amount_per_sqm
-          return {} if net_rent_amount.blank?
-
-          {
-            net_rent_amount: net_rent_amount,
-            net_rent_amount_per_sqm: (net_rent_amount / area).ceil(2),
-          }
-        end
-
-        def gross_rent_amount_per_sqm
-          return {} if gross_rent_amount.blank?
-
-          {
-            gross_rent_amount: gross_rent_amount,
-            gross_rent_amount_per_sqm: (gross_rent_amount / area).ceil(2),
-          }
         end
 
         def net_rent_amount
