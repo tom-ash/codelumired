b39c6e9d7b98cf5ecf3eceb1b451b9575c164389
tom-ash <tomasz.bogusz@outlook.com>
2023-03-11 11:13:04 +0100

    mapawynajmu.pl > user > refactor user edit extended

--- sites/mapawynajmu_pl/app/lib/mapawynajmu_pl/api/tracks/user/edit/appender.rb
+++ sites/mapawynajmu_pl/app/lib/mapawynajmu_pl/api/tracks/user/edit/appender.rb
@@ -13,10 +13,9 @@ module MapawynajmuPl
 
             def control
               {
-                emailStage: nil,
-                emailStep: 'currentEmailVerification',
-                phoneStage: nil,
-                destroyStage: nil,
+                connecting: false,
+                userDeleteCellOpened: false,
+                # emailStep: 'currentEmailVerification',
               }
             end
 
@@ -26,17 +25,25 @@ module MapawynajmuPl
                   cellOpen: 'Zmień',
                   cellClose: 'Anuluj',
                   headingOne: 'Ustawienia konta',
-                  userDeletionCellOpen: 'Usuń konto',
-                  userDeletionTitle: 'Usuwanie konta',
-                  userDeletionValue: 'Usunięcie konta jest nieodwracalne.',
+                  verificationCodeInputLabel: 'Kod werifykacyjny',
+                  verificationCodeInputInvalidError: 'Nieprawidłowy kod weryfikacyjny.',
+                  userDeleteCellOpen: 'Usuń konto',
+                  userDeleteTitle: 'Usuwanie konta',
+                  userDeleteValue: 'Usunięcie konta jest nieodwracalne.',
+                  userDeleteExplanation: "Kod weryfikacyjny został wysłany na adres email przypisany do konta. Wklej go ponizej i naciśnij przycisk 'Usuń konto', aby nieodwracalnie usunąć konto.",
+                  deleteUser: 'Usuń konto',
                 },
                 en: {
                   cellOpen: 'Change',
                   cellClose: 'Cancel',
                   headingOne: 'Account Settings',
-                  userDeletionCellOpen: 'Delete account',
-                  userDeletionTitle: 'Account Deletion',
-                  userDeletionValue: 'Account deletion is irreversible.',
+                  verificationCodeInputLabel: 'Verification Code',
+                  verificationCodeInputInvalidError: 'Invalid verification code.',
+                  userDeleteCellOpen: 'Delete account',
+                  userDeleteTitle: 'Account Deletion',
+                  userDeleteValue: 'Account deletion is irreversible.',
+                  userDeleteExplanation: "The verification code has been sent to the account email address. Paste it below and click 'Delete account' to irreversibly delete the account.",
+                  deleteUser: 'Delete Account',
                 },
               }[lang]
             end
@@ -50,7 +57,6 @@ module MapawynajmuPl
 
             def inputs
               {
-                userDeletionCellOpened: false,
                 country_code: authenticated_user.country_code,
               }
             end
