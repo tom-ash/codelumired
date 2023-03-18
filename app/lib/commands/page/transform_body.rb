7091520bdc384a768af072dd47f99fb4c560541f
tom-ash <tomasz.bogusz@outlook.com>
2023-03-18 10:59:13 +0100

    shared > page > adjust Page::TransformBody

--- app/lib/commands/page/transform_body.rb
+++ app/lib/commands/page/transform_body.rb
@@ -21,7 +21,36 @@ module Commands
       attr_reader :page, :constantized_site_name, :bucket
 
       def transform_body
-        page.body[0]['className'] = 'page'
+        json_body = page.body.to_json
+
+        json_body = json_body.gsub('"id":', '"identifier":')
+        json_body = json_body.gsub('"class_name":', '"className":')
+        json_body = json_body.gsub('"st":', '"style":')
+        json_body = json_body.gsub('"code_lang":', '"codeLang":')
+
+        json_body = json_body.gsub('"header":', '"Header":')
+        json_body = json_body.gsub('"main":', '"Main":')
+        json_body = json_body.gsub('"article":', '"Article":')
+        json_body = json_body.gsub('"section":', '"Section":')
+        json_body = json_body.gsub('"div":', '"Division":')
+        json_body = json_body.gsub('"def":', '"Definition":')
+        json_body = json_body.gsub('"definition":', '"body":')
+        json_body = json_body.gsub('"h1":', '"HeadingOne":')
+        json_body = json_body.gsub('"h2":', '"HeadingTwo":')
+        json_body = json_body.gsub('"h3":', '"HeadingThree":')
+        json_body = json_body.gsub('"heading_one":', '"HeadingOne":')
+        json_body = json_body.gsub('"heading_two":', '"HeadingTwo":')
+        json_body = json_body.gsub('"heading_three":', '"HeadingThree":')
+        json_body = json_body.gsub('"heading_four":', '"HeadingFour":')
+        json_body = json_body.gsub('"editorial":', '"Editorial":')
+        json_body = json_body.gsub('"toc":', '"TableOfContents":')
+        json_body = json_body.gsub('"ul":', '"UnorderedList":')
+        json_body = json_body.gsub('"img":', '"Image":')
+        json_body = json_body.gsub('"image":', '"Image":')
+        json_body = json_body.gsub('"code":', '"Code":')
+        json_body = json_body.gsub('"yte":', '"YouTubeEmbed":')
+
+        page.body = JSON.parse(json_body)
         page.save!
       end
 
