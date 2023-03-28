4c3aefaf6186ddcdf6d4b22d75f3308314b6eb6a
tom-ash <tomasz.bogusz@outlook.com>
2023-03-28 17:25:52 +0200

    mapawynajmu.pl > listing > common > form > refactor Additional

--- sites/mapawynajmu_pl/app/lib/mapawynajmu_pl/api/tracks/announcement/create/form/appender.rb
+++ sites/mapawynajmu_pl/app/lib/mapawynajmu_pl/api/tracks/announcement/create/form/appender.rb
@@ -24,21 +24,35 @@ module MapawynajmuPl
                     categoryHeading: 'Kategoria',
                     categoryInstructions: 'Kliknij w odpowiednią ikonę, aby wybrać kategorię.',
                     areaInputLabel: 'Powierzchnia',
+                    areaInputPlaceholder: 'Podaj powierzchnię',
                     linkInputLabel: 'Link',
                     nameInputLabel: 'Nazwa',
                     showPolishDescriptionCheckboxLabel: 'Dodaj Opis w języku polskim',
                     showEnglishDescriptionCheckboxLabel: 'Dodaj Opis w języku angielskim',
                     showAvailabilityDateCheckboxLabel: 'Dodaj datę dostępności',
+                    floorSelectLabel: 'Piętro',
+                    totalFloorsSelectLabel: 'Liczba pięter budynku',
+                    roomsSelectLabel: 'Liczba pokoi',
+
+                    netRentAmountInputLabel: 'Miesięczna cena netto',
+                    grossRentAmountInputLabel: 'Miesięczna cena',
                   },
                   en: {
                     categoryHeading: 'Category',
                     categoryInstructions: 'Click the respective icon to select the category.',
                     areaInputLabel: 'Area',
+                    areaInputPlaceholder: 'Provide area',
                     linkInputLabel: 'Link',
                     nameInputLabel: 'Name',
                     showPolishDescriptionCheckboxLabel: 'Add Description in Polish Language',
                     showEnglishDescriptionCheckboxLabel: 'Add Description in English Language',
                     showAvailabilityDateCheckboxLabel: 'Add availability date',
+                    floorSelectLabel: 'Floor',
+                    totalFloorsSelectLabel: 'Building Total Floors',
+                    roomsSelectLabel: 'Rooms Amount',
+
+                    netRentAmountInputLabel: 'Monthly Net Price',
+                    grossRentAmountInputLabel: 'Monthly Price',
                   },
                 }[lang].merge(user_form_texts)
               end
@@ -84,6 +98,13 @@ module MapawynajmuPl
                   verificationCode: EMPTY_TEXT,
                   name: EMPTY_TEXT,
                   link: EMPTY_TEXT,
+
+                  floorSelectOptions: floorSelectOptions,
+                  totalFloorsSelectOptions: totalFloorsSelectOptions,
+                  roomsSelectOptions: numberOptions,
+
+                  rentCurrencySelectOptions: rentCurrencyOptions,
+
                 }.merge(user_form_inputs)
               end
 
@@ -93,6 +114,39 @@ module MapawynajmuPl
                   map: NO_ERROR,
                   pictures: NO_ERROR,
                 }.merge(user_form_errors)
+              end
+
+              def floorSelectOptions
+                [
+                  { value: '', text: '' },
+                  { value: -1, text: 'Podziemie' },
+                  { value: 0, text: 'Parter' }
+                ].concat(numberOptions)
+              end
+
+              def totalFloorsSelectOptions
+                [
+                  { value: '', text: '' },
+                  { value: -1, text: 'Tylko Podziemie' },
+                  { value: 0, text: 'Tylko Parter' }
+                ].concat(numberOptions)
+              end
+
+              def rentCurrencyOptions
+                [
+                  { value: 0, text: 'zł' },
+                  { value: 1, text: '€' },
+                  { value: 2, text: '$' },
+                ]
+              end
+
+              def numberOptions
+                Array(1..100).map do |option|
+                  {
+                    value: option,
+                    text: option,
+                  }
+                end
               end
             end
           end
