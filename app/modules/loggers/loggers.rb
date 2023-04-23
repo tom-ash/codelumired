215f976c41bf65d75fa1e7b67ee493b453f676ef
tom-ash <tomasz.bogusz@outlook.com>
2023-04-23 11:41:17 +0200

    shared > remove redundant constants

--- app/modules/loggers/loggers.rb
+++
@@ -1,3 +0,0 @@
-module Loggers
-  # TODO
-end
--- sites/skillfind_tech/app/lib/skillfind_tech/api/methods/job/create/with_user.rb
+++ sites/skillfind_tech/app/lib/skillfind_tech/api/methods/job/create/with_user.rb
@@ -1,1 +1,16 @@
-# TODO!
+# frozen_string_literal: true
+
+module SkillfindTech
+    module Api
+      module Methods
+        module Job
+          module Create
+            class WithUser < Grape::API
+              # TODO
+            end
+          end
+        end
+      end
+    end
+  end
+  
