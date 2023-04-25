77118dd4331483edcad95cd21721768d32a46b13
tom-ash <tomasz.bogusz@outlook.com>
2023-04-25 06:36:08 +0200

    shared > use Sentry only for production

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
-  Rails.env.production? ? SKILLFIND_TECH_URL : SKILLFIND_TECH_URL_DEV
+  return unless Rails.env.production?
 
   config.dsn = 'https://2940536e6132431996571db68fd3a5b1@o876363.ingest.sentry.io/4504761826476032'
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
