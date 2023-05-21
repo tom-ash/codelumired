5010f7e780e7d17c254785dbf9053b27091e0b59
tom-ash <tomasz.bogusz@outlook.com>
2023-05-21 12:47:21 +0200

    shared > page > remove html from IndexNow

--- app/lib/api/page/index_now.rb
+++ app/lib/api/page/index_now.rb
@@ -13,11 +13,9 @@ module Api
 
         page = site::Page.find(params[:id])
 
-        uri = URI("https://www.bing.com/indexnow?url=#{domain_url}/#{page.url}.html&key=#{index_now_key}")
+        uri = URI("https://www.bing.com/indexnow?url=#{domain_url}/#{page.url}&key=#{index_now_key}")
 
-        puts uri
-
-        puts Net::HTTP.get_response(uri)
+        Net::HTTP.get_response(uri)
       end
     end
   end
