cde34d0b772e929f2f80230e58ff12f60d5f218f
tom-ash <tomasz.bogusz@outlook.com>
2023-07-29 09:02:23 +0200

    mapawynajmu.pl > user > disallow signing in by a deleted user

--- app/lib/api/user/authorize/email_and_password.rb
+++ app/lib/api/user/authorize/email_and_password.rb
@@ -4,6 +4,8 @@ module Api
   module User
     module Authorize
       class EmailAndPassword < Grape::API
+        class UserDeletedError < StandardError; end
+
         desc 'Signs in a User with Email & Password.'
         params do
           requires :email, type: String, desc: 'User\'s email.'
@@ -14,6 +16,8 @@ module Api
             params.merge(constantized_site_name: constantized_site_name),
           ).call
 
+          raise UserDeletedError if user.deleted_at?
+
           accessToken = ::Ciphers::Jwt::Encoder.new(id: user.id).call
           href = ::MapawynajmuPl::Api::Tracks::Root::Linker.new(lang.to_sym).call[:href]
 
@@ -21,7 +25,8 @@ module Api
             accessToken: accessToken,
             href: href,
           }
-        rescue ActiveRecord::RecordNotFound, ::Commands::User::Authorize::EmailAndPassword::PasswordError
+        rescue UserDeletedError, ActiveRecord::RecordNotFound, ::Commands::User::Authorize::EmailAndPassword::PasswordError
+          # TODO!: Change 400 to 422.
           error!('Invalid email or password.', 400)
         end
       end
--- spec/app/lib/api/user/authorize/email_and_password_spec.rb
+++ spec/app/lib/api/user/authorize/email_and_password_spec.rb
@@ -1,35 +1,65 @@
-# # frozen_string_literal: true
+# frozen_string_literal: true
 
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe ::Api::User::Authorize::EmailAndPassword do
-#   let(:email) { 'test@example.net' }
-#   let(:password) { 'test_password' }
-#   let(:first_name) { 'Gandalf' }
-#   let(:verification_code) { '3067' }
-#   let(:verification) { [{ namespace: namespace, generated_at: '2021-03-23T08:07:13.189+01:00', encrypted_code: "a1R4FeRxjAJCddgqVV96Nrw73z2w6dGMkMRaaHZrxuaDOC+MC3TeQ09gj5tJ\nzQ4Q\n", encrypted_code_iv: "PRBFoeHuixemdQmc5X6C2Q==\n" }] }
-#   let(:namespace) { 'user/update/password' }
+RSpec.describe ::Api::User::Authorize::EmailAndPassword do
+  let(:email) { 'test@example.net' }
+  let(:password) { 'test_password' }
+  let(:headers) { { 'Lang': 'pl' } }
 
-#   describe 'PUT' do
-#     let!(:user) { create(:mapawynajmu_pl_user, email: email, hashed_password: hashed_password, password_salt: password_salt, first_name: first_name) }
-#     let(:hashed_password) { '$2a$12$xNgbSunb3cSQmHKpSyUT.uFj9Msr8/Fuwngwir/.x65EKvrLzWsVy' }
-#     let(:password_salt) { '4f5d08e79c0987488a78577dc8e365621539e0e1a6bc333aee20d997809cd694' }
+  describe 'PUT' do
+    let(:hashed_password) { '$2a$12$xNgbSunb3cSQmHKpSyUT.uFj9Msr8/Fuwngwir/.x65EKvrLzWsVy' }
+    let(:password_salt) { '4f5d08e79c0987488a78577dc8e365621539e0e1a6bc333aee20d997809cd694' }
 
-#     before { put '/mapawynajmu_pl/user/auth/email-and-password', params: params }
+    context 'when user is active' do
+      context 'when email and password are valid' do
+        let(:params) { { email: email, password: password } }
 
-#     context 'when email and password are correct' do
-#       let(:params) { { email: email, password: password } }
-#       it 'returns :ok (200) response' do
-#         expect(response.status).to eq(200)
-#       end
+        before do
+          create(
+            :mapawynajmu_pl_user,
+            email: email,
+            hashed_password: hashed_password,
+            password_salt: password_salt,
+          )
 
-#       it 'returns the signed in user data' do
-#         expect(JSON.parse(response.body).symbolize_keys).to eq(
-#           accountType: nil,
-#           accessToken: 'ba79099602b5ca78b2c44b652ebee8c8ff0c522e93ab8a7a39ceeb60c3dc40d5fe9984cb45fe4464b4d48c3d6eba70040e6c1f9987c7177f259beee19dcd1a1e',
-#           name: 'Gandalf'
-#         )
-#       end
-#     end
-#   end
-# end
+          put '/mapawynajmu-pl/user/auth/email-and-password', params: params, headers: headers
+        end
+
+        it 'returns :ok (200) response' do
+          expect(response.status).to eq(200)
+        end
+    
+        it 'returns the signed in user data' do
+          expect(JSON.parse(response.body).symbolize_keys).to include(
+            accessToken: be_an_instance_of(String),
+            href: 'http://local.mapawynajmu.pl:8080/'
+          )
+        end
+      end
+    end
+
+    context 'when user is deleted' do
+      context 'when email and password are valid' do
+        let(:params) { { email: email, password: password } }
+
+        before do
+          create(
+            :mapawynajmu_pl_user,
+            email: email,
+            hashed_password: hashed_password,
+            password_salt: password_salt,
+            deleted_at: Time.now,
+          )
+
+          put '/mapawynajmu-pl/user/auth/email-and-password', params: params, headers: headers
+        end
+
+        # TODO!: Change 400 to 422.
+        it 'returns :bad_request (400) response' do
+          expect(response.status).to eq(400)
+        end
+      end
+    end
+  end
+end
