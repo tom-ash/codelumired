b20798fd9ce1b03b923f6533fc811143a135fb48
tom-ash <tomasz.bogusz@outlook.com>
2023-09-26 18:01:45 +0200

    mapawynajmu.pl > listing > add create with user spec

--- sites/mapawynajmu_pl/app/lib/mapawynajmu_pl/api/listing/helpers/attrs.rb
+++ sites/mapawynajmu_pl/app/lib/mapawynajmu_pl/api/listing/helpers/attrs.rb
@@ -10,10 +10,13 @@ module MapawynajmuPl
           params :announcement_attrs do
             requires :announcement, type: Hash do
               requires :category, type: Integer
-              requires :area, type: Float
-              requires :pictures, type: Array
               requires :latitude, type: Float
               requires :longitude, type: Float
+              requires :pictures, type: Array
+              optional :locality, type: String
+              optional :sublocality, type: String
+              optional :route, type: String
+              optional :area, type: Float
               optional :rent_currency, type: Integer
               optional :net_rent_amount, type: Float
               optional :gross_rent_amount, type: Float
@@ -25,9 +28,6 @@ module MapawynajmuPl
               optional :furnishings, type: Array
               optional :polish_description, type: String
               optional :english_description, type: String
-              optional :locality, type: String
-              optional :sublocality, type: String
-              optional :route, type: String
               optional :add_promotion, type: Boolean
             end
           end
---
+++ spec/mapawynajmu_pl/api/methods/visitor/listings/create_spec.rb
@@ -0,0 +1,127 @@
+# frozen_string_literal: true
+
+require 'mapawynajmu_pl/rails_helper.rb'
+
+::MapawynajmuPl::Api::Listing::Create::WithUser
+
+RSpec.describe ::MapawynajmuPl::Api::Listing::Create::WithUser do
+  let(:create_listing_as_visitor) do
+    post '/mapawynajmu-pl/announcement/create/with-user', headers: headers, params: params
+  end
+  let(:headers) do
+    {
+      'Lang': 'pl',
+    }
+  end
+  let(:params) do
+    {
+      announcement: listing_attributes,
+      user: user_attributes,
+    }
+  end
+
+  before do
+    allow_any_instance_of(PersistedObject).to receive(:move_to)
+  end
+
+  describe '/announcement/create/with-user' do
+    describe 'listing attributes' do
+      let(:user_attributes) do
+        {
+          # account_type # TODO!!!
+          email_address: 'test@example.net',
+          password: 'test-password',
+          country_code: '+48',
+          phone_number: '500100200',
+          terms_of_service_consent: true,
+          consents: [
+            {
+              type: 'terms_of_service',
+              granted: true,
+              displayed_text: 'Oświadczam, że znam i akceptuję postanowienia Regulaminu mapawynajmu.pl.'
+            }
+          ]
+        }
+      end
+
+      describe 'required attributes' do
+        context 'when required attributes are present & valid' do  
+          let(:listing_attributes) do
+            {
+              category: 0,
+              latitude: 42,
+              longitude: 42,
+              pictures: [
+                {
+                  database: '20230925173842604QpWEWHxp-xQG4FoAR0drEA.jpeg',
+                  description: ''
+                }
+              ],
+              add_promotion: false,
+            }
+          end
+  
+          it 'returns :ok (201)' do
+            create_listing_as_visitor
+            expect(response.status).to eq(201)
+          end
+
+          it 'creates a new listing' do
+            expect { create_listing_as_visitor }.to change { ::MapawynajmuPl::Listing.count }.by(1)
+          end
+
+          it 'creates a new user' do
+            expect { create_listing_as_visitor }.to change { ::MapawynajmuPl::User.count }.by(1)
+          end
+        end
+      end
+
+      describe 'optional attributes' do
+        context 'when optional attributes are present & valid' do  
+          let(:listing_attributes) do
+            {
+              category: 0,
+              latitude: 42,
+              longitude: 42,
+              pictures: [
+                {
+                  database: '20230925173842604QpWEWHxp-xQG4FoAR0drEA.jpeg',
+                  description: ''
+                }
+              ],
+              locality: 'Warszawa',
+              sublocality: 'Ochota',
+              route: 'Aleje Jerozolimskie',
+              area: 42.42,
+              rent_currency: 0,
+              net_rent_amount: 100,
+              gross_rent_amount: 123,
+              rooms: 2,
+              floor: 23,
+              total_floors: 42,
+              features: ["document_scanning", "correspondence_service", "conference_hall_access"], # TODO!
+              furnishings: ["fireprotectionsystem", "airconditioning", "alarm system"], # TODO!
+              availability_date: '2023-09-26',
+              polish_description: 'Description in Polish.',
+              english_description: 'Description in English.',
+              add_promotion: false,
+            }
+          end
+  
+          it 'returns :ok (201)' do
+            create_listing_as_visitor
+            expect(response.status).to eq(201)
+          end
+
+          it 'creates a new listing' do
+            expect { create_listing_as_visitor }.to change { ::MapawynajmuPl::Listing.count }.by(1)
+          end
+
+          it 'creates a new user' do
+            expect { create_listing_as_visitor }.to change { ::MapawynajmuPl::User.count }.by(1)
+          end
+        end
+      end
+    end
+  end
+end
