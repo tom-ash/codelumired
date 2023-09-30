8456593d0d8063151a58035ee167906fe48d450c
tom-ash <tomasz.bogusz@outlook.com>
2023-09-30 16:19:42 +0200

    mapawynajmu.pl > users > verify > extend spec

--- spec/mapawynajmu_pl/api/methods/user/verify_spec.rb
+++ spec/mapawynajmu_pl/api/methods/user/verify_spec.rb
@@ -57,15 +57,43 @@ RSpec.describe ::Api::User::Create::EmailAndPassword do
 
         context 'when the user has a listing' do
           let!(:listing) { create(:mapawynajmu_pl_listing, user: user, user_verified: false) }
+          let(:mail_message) { instance_double(ActionMailer::MessageDelivery, deliver_now: true) }
+
+          before do
+            allow(TransactionalMailer).to receive(:listing_confirmation_email).and_return(mail_message)
+          end
 
           it 'marks the listing as verified' do
             expect { verify_user }.to change { listing.reload.user_verified }.from(false).to(true)
           end
 
-          it 'responds with listing confirmation href' do
-            verify_user
+          it 'sends listing confirmation email' do
+            expect(TransactionalMailer).to receive(:listing_confirmation_email)
+            expect(mail_message).to receive(:deliver_now)
 
-            expect(body[:href]).to eq("http://local.mapawynajmu.pl:8080/podsumowanie-dodanego-ogloszenia/#{listing.id}")
+            verify_user
+          end
+
+          context 'when the listing is not promoted' do
+            it 'responds with listing confirmation href' do
+              verify_user
+  
+              expect(body[:href]).to eq("http://local.mapawynajmu.pl:8080/podsumowanie-dodanego-ogloszenia/#{listing.id}")
+            end
+          end
+
+          context 'when the listing is promoted' do
+            let!(:listing) { create(:mapawynajmu_pl_listing, user: user, user_verified: false, is_promoted: true) }
+
+            it 'responds with payment href' do
+              verify_user
+  
+              expect(body[:href]).to match(/^https:\/\/merch-prod.snd.payu.com\/pay\/\?orderId=/)
+            end
+
+            it 'creates an order with status PENDING' do
+              expect { verify_user }.to change { ::MapawynajmuPl::Order.count }.by(1)
+            end
           end
         end
 
