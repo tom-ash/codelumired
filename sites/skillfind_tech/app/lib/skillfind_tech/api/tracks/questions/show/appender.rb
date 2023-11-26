ecbae2290704db966c678c94b9f0fdd1b9d2dbed
tom-ash <tomasz.bogusz@outlook.com>
2023-11-26 12:27:46 +0100

    skillfind.tech > visitor > questions > show > use database data

--- sites/skillfind_tech/app/lib/skillfind_tech/api/tracks/questions/show/appender.rb
+++ sites/skillfind_tech/app/lib/skillfind_tech/api/tracks/questions/show/appender.rb
@@ -11,18 +11,22 @@ module SkillfindTech
             private
 
             def data
+              match_data
               {
-                title: 'npm start',
-                body: 'What is the purpose of the "npm start" command in a Node.js project?',
-                hint: 'https://docs.npmjs.com/cli/v7/commands/npm-start',
-                answers: [
-                  'To install new npm packages globally.',
-                  'To start the Node.js REPL (Read-Eval-Print Loop).',
-                  'To run the tests defined in the project.',
-                  'To start the application as defined in the "scripts" section of the package.json file.',
-                ],
-                explanation: 'The "npm start" command is a script defined in the "scripts" section of the package.json file in a Node.js project. This script is typically used to start the application. When you run "npm start," npm looks for a script named "start" in the "scripts" section and executes the command specified there. It is a common convention to use "npm start" for starting the main application in a project. This is particularly useful for web applications, servers, or any other Node.js application where starting the application is a common task during development or deployment.',
+                title: question.title,
+                body: question.body,
+                answers: question.answers,
+                hint: question.hint,
+                explanation: question.explanation,
               }
+            end
+
+            def question
+              @question ||= ::SkillfindTech::Question.find_by(url: question_path)
+            end
+
+            def question_path
+              @question_path ||= match_data[:question_path]
             end
           end
         end
