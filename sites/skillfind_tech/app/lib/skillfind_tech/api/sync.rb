af8bab7d1489030393c166717824fe3e6da452c3
Tomasz Bogusz <tomaszbogusz@outlook.com>
2024-07-06 06:54:46 +0200

    feat(skillfind.tech/visitor): add about

--- sites/skillfind_tech/app/lib/skillfind_tech/api/sync.rb
+++ sites/skillfind_tech/app/lib/skillfind_tech/api/sync.rb
@@ -12,17 +12,14 @@ module SkillfindTech
             ::SkillfindTech::Api::Tracks::Visitor::PrivacyPolicy::Meta::TRACK => ::SkillfindTech::Api::Tracks::Visitor::PrivacyPolicy::Meta::UNLOCALIZED_PATH,
             ::SkillfindTech::Api::Tracks::Visitor::CookiesPolicy::Meta::TRACK => ::SkillfindTech::Api::Tracks::Visitor::CookiesPolicy::Meta::UNLOCALIZED_PATH,
             ::SkillfindTech::Api::Tracks::Visitor::PrivacySettings::Meta::TRACK => ::SkillfindTech::Api::Tracks::Visitor::PrivacySettings::Meta::UNLOCALIZED_PATH,
+            ::SkillfindTech::Api::Tracks::Visitor::About::Meta::TRACK => ::SkillfindTech::Api::Tracks::Visitor::About::Meta::UNLOCALIZED_PATH,
             ::SkillfindTech::Api::Tracks::User::New::Form::Meta::TRACK => ::SkillfindTech::Api::Tracks::User::New::Form::Meta::UNLOCALIZED_PATH,
-
             ::SkillfindTech::Api::Tracks::User::New::Verify::Meta::TRACK => ::SkillfindTech::Api::Tracks::User::New::Verify::Meta::UNLOCALIZED_PATH,
-
             ::SkillfindTech::Api::Tracks::User::Auth::Meta::TRACK => ::SkillfindTech::Api::Tracks::User::Auth::Meta::UNLOCALIZED_PATH,
             ::SkillfindTech::Api::Tracks::User::ResetPassword::Meta::TRACK => ::SkillfindTech::Api::Tracks::User::ResetPassword::Meta::UNLOCALIZED_PATH,
             ::SkillfindTech::Api::Tracks::Posting::Create::Form::Meta::TRACK => ::SkillfindTech::Api::Tracks::Posting::Create::Form::Meta::UNLOCALIZED_PATH,
             ::SkillfindTech::Api::Tracks::Posting::Create::Summary::Meta::TRACK => ::SkillfindTech::Api::Tracks::Posting::Create::Summary::Meta::UNLOCALIZED_PATH,
-
             ::SkillfindTech::Api::Tracks::Posting::Show::Meta::TRACK => ::SkillfindTech::Api::Tracks::Posting::Show::Meta::UNLOCALIZED_PATH,
-
             ::SkillfindTech::Api::Tracks::Page::Index::Meta::TRACK => ::SkillfindTech::Api::Tracks::Page::Index::Meta::UNLOCALIZED_PATH,
             ::SkillfindTech::Api::Tracks::Page::Edit::Meta::TRACK => ::SkillfindTech::Api::Tracks::Page::Edit::Meta::UNLOCALIZED_PATH,
             ::SkillfindTech::Api::Tracks::Image::Index::Meta::TRACK => ::SkillfindTech::Api::Tracks::Image::Index::Meta::UNLOCALIZED_PATH,
@@ -30,16 +27,9 @@ module SkillfindTech
             ::SkillfindTech::Api::Tracks::Assets::Index::Meta::TRACK => ::SkillfindTech::Api::Tracks::Assets::Index::Meta::UNLOCALIZED_PATH,
             ::SkillfindTech::Api::Tracks::Redirects::Index::Meta::TRACK => ::SkillfindTech::Api::Tracks::Redirects::Index::Meta::UNLOCALIZED_PATH,
             ::SkillfindTech::Api::Tracks::Questions::Show::Meta::TRACK => ::SkillfindTech::Api::Tracks::Questions::Show::Meta::UNLOCALIZED_PATH,
-
             ::SkillfindTech::Api::Tracks::User::Postings::Index::Meta::TRACK => ::SkillfindTech::Api::Tracks::User::Postings::Index::Meta::UNLOCALIZED_PATH,
-
             ::SkillfindTech::Api::Tracks::User::Postings::Edit::Meta::TRACK => ::SkillfindTech::Api::Tracks::User::Postings::Edit::Meta::UNLOCALIZED_PATH,
-
             ::SkillfindTech::Api::Tracks::User::Users::Edit::Meta::TRACK => ::SkillfindTech::Api::Tracks::User::Users::Edit::Meta::UNLOCALIZED_PATH,
-
-
-
-            
           }
         end
 
@@ -53,17 +43,14 @@ module SkillfindTech
           when ::SkillfindTech::Api::Tracks::Visitor::PrivacyPolicy::Meta::TRACK then ::SkillfindTech::Api::Tracks::Visitor::PrivacyPolicy::Appender.new(attrs).call
           when ::SkillfindTech::Api::Tracks::Visitor::CookiesPolicy::Meta::TRACK then ::SkillfindTech::Api::Tracks::Visitor::CookiesPolicy::Appender.new(attrs).call
           when ::SkillfindTech::Api::Tracks::Visitor::PrivacySettings::Meta::TRACK then ::SkillfindTech::Api::Tracks::Visitor::PrivacySettings::Appender.new(attrs).call
+          when ::SkillfindTech::Api::Tracks::Visitor::About::Meta::TRACK then ::SkillfindTech::Api::Tracks::Visitor::About::Appender.new(attrs).call
           when ::SkillfindTech::Api::Tracks::User::New::Form::Meta::TRACK then ::SkillfindTech::Api::Tracks::User::New::Form::Appender.new(attrs).call
-
           when ::SkillfindTech::Api::Tracks::User::New::Verify::Meta::TRACK then ::SkillfindTech::Api::Tracks::User::New::Verify::Appender.new(attrs).call
-
           when ::SkillfindTech::Api::Tracks::User::Auth::Meta::TRACK then ::SkillfindTech::Api::Tracks::User::Auth::Appender.new(attrs).call
           when ::SkillfindTech::Api::Tracks::User::ResetPassword::Meta::TRACK then ::SkillfindTech::Api::Tracks::User::ResetPassword::Appender.new(attrs).call
           when ::SkillfindTech::Api::Tracks::Posting::Create::Form::Meta::TRACK then ::SkillfindTech::Api::Tracks::Posting::Create::Form::Appender.new(attrs).call
           when ::SkillfindTech::Api::Tracks::Posting::Create::Summary::Meta::TRACK then ::SkillfindTech::Api::Tracks::Posting::Create::Summary::Appender.new(attrs).call
-
           when ::SkillfindTech::Api::Tracks::Posting::Show::Meta::TRACK then ::SkillfindTech::Api::Tracks::Posting::Show::Appender.new(attrs).call
-
           when ::SkillfindTech::Api::Tracks::Page::Index::Meta::TRACK then ::SkillfindTech::Api::Tracks::Page::Index::Appender.new(attrs).call
           when ::SkillfindTech::Api::Tracks::Page::Show::Meta::TRACK then ::SkillfindTech::Api::Tracks::Page::Show::Appender.new(**attrs, page: page).call
           when ::SkillfindTech::Api::Tracks::Page::Edit::Meta::TRACK then ::SkillfindTech::Api::Tracks::Page::Edit::Appender.new(attrs).call
@@ -74,12 +61,8 @@ module SkillfindTech
           when ::Api::Tracks::Redirect::Show::Meta::TRACK then ::Api::Tracks::Redirect::Show::Appender.new(attrs).call
           when ::SkillfindTech::Api::Tracks::Questions::Show::Meta::TRACK then ::SkillfindTech::Api::Tracks::Questions::Show::Appender.new(attrs).call
           when ::SkillfindTech::Api::Tracks::User::Postings::Index::Meta::TRACK then ::SkillfindTech::Api::Tracks::User::Postings::Index::Appender.new(attrs).call
-
           when ::SkillfindTech::Api::Tracks::User::Postings::Edit::Meta::TRACK then ::SkillfindTech::Api::Tracks::User::Postings::Edit::Appender.new(attrs).call
-
           when ::SkillfindTech::Api::Tracks::User::Users::Edit::Meta::TRACK then ::SkillfindTech::Api::Tracks::User::Users::Edit::Appender.new(attrs).call
-
-            
           end
         end
 
@@ -93,6 +76,7 @@ module SkillfindTech
             ::SkillfindTech::Api::Tracks::Visitor::PrivacyPolicy::Meta::TRACK => ::SkillfindTech::Api::Tracks::Visitor::PrivacyPolicy::Linker.new(lang).call,
             ::SkillfindTech::Api::Tracks::Visitor::CookiesPolicy::Meta::TRACK => ::SkillfindTech::Api::Tracks::Visitor::CookiesPolicy::Linker.new(lang).call,
             ::SkillfindTech::Api::Tracks::Visitor::PrivacySettings::Meta::TRACK => ::SkillfindTech::Api::Tracks::Visitor::PrivacySettings::Linker.new(lang).call,
+            ::SkillfindTech::Api::Tracks::Visitor::About::Meta::TRACK => ::SkillfindTech::Api::Tracks::Visitor::About::Linker.new(lang).call,
             ::SkillfindTech::Api::Tracks::User::ResetPassword::Meta::TRACK => ::SkillfindTech::Api::Tracks::User::ResetPassword::Linker.new(lang).call,
             ::SkillfindTech::Api::Tracks::User::New::Form::Meta::TRACK => ::SkillfindTech::Api::Tracks::User::New::Form::Linker.new(lang).call,
             ::SkillfindTech::Api::Tracks::User::Auth::Meta::TRACK => ::SkillfindTech::Api::Tracks::User::Auth::Linker.new(lang).call,
@@ -102,16 +86,10 @@ module SkillfindTech
             ::SkillfindTech::Api::Tracks::Image::Index::Meta::TRACK => ::SkillfindTech::Api::Tracks::Image::Index::Linker.new(lang).call,
             ::SkillfindTech::Api::Tracks::SocialPages::Facebook::Meta::TRACK => ::SkillfindTech::Api::Tracks::SocialPages::Facebook::Linker.new(lang).call,
             ::SkillfindTech::Api::Tracks::SocialPages::Linkedin::Meta::TRACK => ::SkillfindTech::Api::Tracks::SocialPages::Linkedin::Linker.new(lang).call,
-
             ::SkillfindTech::Api::Tracks::SocialPages::Linkedin::Meta::TRACK => ::SkillfindTech::Api::Tracks::SocialPages::Linkedin::Linker.new(lang).call,
-
             ::SkillfindTech::Api::Tracks::User::Users::Show::Meta::TRACK => ::SkillfindTech::Api::Tracks::User::Users::Show::Linker.new(lang).call,
-
             ::SkillfindTech::Api::Tracks::User::Users::Edit::Meta::TRACK => ::SkillfindTech::Api::Tracks::User::Users::Edit::Linker.new(lang).call,
-
             ::SkillfindTech::Api::Tracks::User::Postings::Index::Meta::TRACK => ::SkillfindTech::Api::Tracks::User::Postings::Index::Linker.new(lang).call,
-
-
           )
         end
 
